2ca67bfd27a4c0e26012adb3cd63fb36f17cb534
Yarockii Daniil <yarockiidaniil.mobile@gmail.com>
2018-03-27 11:35:36 +0300

    repaired the work of the first program

--- lab_01_1_1/main.c
+++ lab_01_1_1/main.c
@@ -3,14 +3,14 @@
 # define PI 3.14
 int main()
 {
-	setbuf(stdout, NULL);
-	float down, upper, ang, plos ;
-	printf("Down side: ");
-	scanf("%f", &down);
-	printf("Upper side: ");
-	scanf("%f", &upper);
-	printf("Angular: ");
-	scanf("%f", &ang);
-    plos = abs(0.5*(down*down-upper*upper)*tan(ang));
+    setbuf(stdout, NULL);
+    float down, upper, ang, plos ;
+    printf("Down side: ");
+    scanf("%f", &down);
+    printf("Upper side: ");
+    scanf("%f", &upper);
+    printf("Angular: ");
+    scanf("%f", &ang);
+    plos = abs(0.5*(down*down-upper*upper)*tan((ang*PI)/180));
     printf("S = %.4f", plos);
 }
