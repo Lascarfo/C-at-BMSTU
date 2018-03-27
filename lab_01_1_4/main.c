1cd17b5d7059135921d4abf05c5481cad50d5f83
Yarockii Daniil <yarockiidaniil.mobile@gmail.com>
2018-03-27 11:52:39 +0300

    change spaces to tabs, i so sheepish

--- lab_01_1_4/main.c
+++ lab_01_1_4/main.c
@@ -5,12 +5,12 @@
 # define KV 3
 int main(void)
 {
-    int number, pod, st;
-    setbuf (stdout, NULL);
-    printf("input room's number: ");
-    scanf("%d", &number);
-    pod = (number + SF) / SS;
-    number = number - (pod  - 1 ) * SS;
-    st = (( number + KV ) / KVO);
-    printf("podjezd is: %d, stage is: %d", pod, st);
+	int number, pod, st;
+	setbuf (stdout, NULL);
+	printf("input room's number: ");
+	scanf("%d", &number);
+	pod = (number + SF) / SS;
+	number = number - (pod  - 1 ) * SS;
+	st = (( number + KV ) / KVO);
+	printf("podjezd is: %d, stage is: %d", pod, st);
 }
