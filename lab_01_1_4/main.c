fc26a7a4bad65bd85ca39fa21b6e835479ed20e3
Yarockii Daniil <yarockiidaniil.mobile@gmail.com>
2018-03-27 13:12:54 +0300

    regular updates

--- lab_01_1_4/main.c
+++ lab_01_1_4/main.c
@@ -1,16 +1,17 @@
 # include <stdio.h>
+
 # define SS 36
 # define SF 35
 # define KVO 4
 # define KV 3
 int main(void)
 {
-    int number,pod,st;
-    setbuf(stdout,NULL);
-    printf("input room's number: ");
-    scanf("%d",&number);
-    pod=(number +SF)/SS;
-    number=number-(pod-1)*SS;
-    st=((number+KV)/KVO);
-    printf("podjezd is: %d, stage is: %d",pod,st);
+    int number, pod, st;
+    setbuf ( stdout, NULL );
+    printf ( "input room's number: " );
+    scanf ( "%d",&number );
+    pod = ( number + SF ) / SS;
+    number = number - ( pod - 1 ) * SS;
+    st= (( number + KV ) / KVO );
+    printf ( "podjezd is: %d, stage is: %d", pod, st );
 }
