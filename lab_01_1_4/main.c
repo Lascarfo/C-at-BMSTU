ff4aafa0f10218ce19b631693fff46d8c72d460e
Yarockii Daniil <yarockiidaniil.mobile@gmail.com>
2018-03-27 13:24:23 +0300

    regular updates

--- lab_01_1_4/main.c
+++ lab_01_1_4/main.c
@@ -7,11 +7,11 @@
 int main(void)
 {
     int number, pod, st;
-    setbuf ( stdout, NULL );
+    setbuf(stdout, NULL);
     printf ( "input room's number: " );
-    scanf ( "%d",&number );
-    pod = ( number + SF ) / SS;
-    number = number - ( pod - 1 ) * SS;
-    st= (( number + KV ) / KVO );
-    printf ( "podjezd is: %d, stage is: %d", pod, st );
+    scanf("%d", &number);
+    pod = (number + SF ) / SS;
+    number = number -(pod - 1) * SS;
+    st = ((number + KV) / KVO );
+    printf ("podjezd is: %d, stage is: %d", pod, st);
 }
