2ef80b115a2ab7f5c5faf3d6729dd98ba9870be3
Yarockii Daniil <yarockiidaniil.mobile@gmail.com>
2018-03-27 12:10:11 +0300

    minor updates

--- lab_01_1_1/main.c
+++ lab_01_1_1/main.c
@@ -1,17 +1,17 @@
-# include <stdio.h>
-# include <stdlib.h>
-# include <math.h>
-# define PI 3.14
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define PI 3.14
 int main()
 {
-	setbuf(stdout, NULL);
-	float down, upper, ang, plos ;
-	printf("Down side: ");
-	scanf("%f", &down);
-	printf("Upper side: ");
-	scanf("%f", &upper);
-	printf("Angular: ");
-	scanf("%f", &ang);
-   	plos = abs(0.5*(down*down-upper*upper)*tan((ang*PI)/180));
-   	printf("S = %.4f", plos);
+    setbuf(stdout, NULL);
+    float down, upper, ang, plos ;
+    printf("Down side: ");
+    scanf("%f", &down);
+    printf("Upper side: ");
+    scanf("%f", &upper);
+    printf("Angular: ");
+    scanf("%f", &ang);
+    plos = fabs(0.5*(down*down-upper*upper)*tan((ang*PI)/180));
+    printf("S = %.4f", plos);
 }
