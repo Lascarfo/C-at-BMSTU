b29cf25a926b5951d06ab01b7a3a03dfdfac8e89
Yarockii Daniil <yarockiidaniil.mobile@gmail.com>
2018-03-27 15:17:10 +0300

    try 36

--- lab_01_1_1/main.c
+++ lab_01_1_1/main.c
@@ -6,12 +6,11 @@
 
 int main(void)
 {
-    setbuf(stdout, NULL);
     int ang;
     float down, upper, plos, radians;
     printf("Down side, upper side, angular: ");
     scanf("%f %f %d", &down, &upper, &ang);
     radians = (ang * PI) / FULLANGLE;
     plos = fabs(((down + upper) * tan(radians)) / 2);
-    printf("S = %.4f", plos);
+    printf("S = %.4f\n", plos);
 }
