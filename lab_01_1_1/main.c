70b7421ea8e90d2f8d39e74e364b999387fe1e5a
Yarockii Daniil <yarockiidaniil.mobile@gmail.com>
2018-03-27 14:25:01 +0300

    try numer 28, i think this is last

--- lab_01_1_1/main.c
+++ lab_01_1_1/main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
-#define PI 3.14
-#define ANGULAR 0.01745329251
+#define PI 3.14159265359
 
 int main()
 {
     setbuf(stdout, NULL);
-    float down, upper, ang, plos;
+    float down, upper, ang, plos, radians;
     printf("Down side: ");
     scanf("%f", &down);
     printf("Upper side: ");
     scanf("%f", &upper);
     printf("Angular: ");
     scanf("%f", &ang);
-    plos = 0.5 * (down + upper) * tan(ang * ANGULAR);
+    radians = (ang * PI) / 180.0;
+    plos = 0.5 * (down + upper)* tan(radians);
     printf("S = %.4f", plos);
 }
