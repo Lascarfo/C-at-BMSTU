d4d65be50c88c569e3b3cf3d13efb8882aa79366
Yarockii Daniil <yarockiidaniil.mobile@gmail.com>
2018-03-27 11:45:57 +0300

    defines added

--- lab_01_1_4/main.c
+++ lab_01_1_4/main.c
@@ -1,14 +1,16 @@
 # include <stdio.h>
 # define SS 36
 # define SF 35
+# define KVO 4
+# define KV 3
 int main(void)
 {
     int number, pod, st;
-	setbuf (stdout, NULL);
-	printf("input room's number: ");
-	scanf("%d", &number);
+    setbuf (stdout, NULL);
+    printf("input room's number: ");
+    scanf("%d", &number);
     pod = (number + SF) / SS;
-    number = number - (pod  -1 ) * SS;
-	st = (( number + 3 ) / 4);
-	printf("podjezd is: %d, stage is: %d", pod, st);
+    number = number - (pod  - 1 ) * SS;
+    st = (( number + KV ) / KVO);
+    printf("podjezd is: %d, stage is: %d", pod, st);
 }
