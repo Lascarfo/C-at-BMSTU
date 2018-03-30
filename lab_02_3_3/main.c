fc841bdbb73bc9c1df7725443c8469d7d8ed7318
Daniil Yarockii <yarockiidaniil.mobile@gmail.com>
2018-03-30 03:35:56 +0300

    Complete task #3, test data added,  all tasks complete!

---
+++ lab_02_3_3/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <math.h>
+
+#define OK 0
+#define ERR_IO 1
+#define ERR_RANGE 2
+#define MINUS -1
+
+float calc_s(float eps, float x)
+{
+   float changing_x = x;
+   float sum = 0.0;
+   int numer = 3;
+   while (fabs(changing_x) >= eps)
+   {
+       sum += changing_x;
+       changing_x = MINUS * (pow(x, numer) / numer);
+       numer += 2;
+   }
+   return sum;
+}
+
+int main(void)
+{
+    int rc = OK;
+    float eps, x, s, f, abs_f, relative;
+
+    printf("Input eps (0 < eps <= 1) && x (|x| <= 1): ");
+    if (scanf("%f%f", &eps, &x) == 2)
+    {
+        if ((eps > 0) && (eps <= 1) && (fabs(x) <= 1))
+        {
+            f = atan(x);
+            s = calc_s(eps, x);
+            abs_f = fabs(f - s);
+            relative = abs_f / fabs(f);
+            printf("s(x) = %f\nf(x) = %f\n", s, f);
+            printf("abs, relative: %f, %f\n", abs_f, relative);
+        }
+        else
+        {
+            rc = ERR_RANGE;
+            printf("required next conditions: 0 < eps <= 1, |x| <= 1 \n");
+        }
+    }
+    else
+    {
+        printf("Incorrect type of value\n");
+        rc = ERR_IO;
+    }
+    return rc;
+}
