60d7d649c6ca8c16c2e2382bf5517d6d01fc22cf
Daniil Yarockii <yarockiidaniil.mobile@gmail.com>
2018-04-08 23:19:16 +0300

    back to roots

--- lab_02_3_3/main.c
+++ lab_02_3_3/main.c
@@ -4,7 +4,6 @@
 #define OK 0
 #define ERR_IO 1
 #define ERR_RANGE ERR_IO + 1
-#define ERR_INPUT ERR_RANGE + 1
 #define MINUS -1
 
 float calc_s(float eps, float x)
@@ -26,46 +25,24 @@ int main(void)
 {
     int rc = OK;
     float eps, x, s, f, abs_f, relative;
-    if (scanf("%f", &x) == 1)
+    if (scanf("%f %f", &x, &eps) == 2)
     {
-        if (fabs(x) <= 1)
+        if ((eps > 0) && (eps < 1) && (fabs(x) <= 1) && (x != 0 ))
         {
-            if (scanf("%f", &eps) == 1)
-            {
-                if ((eps > 0) && (eps < 1))
-                {
-                    if ((eps > 0) && (eps < 1) && (fabs(x) <= 1) && (x != 0))
-                    {
-                        f = atan(x);
-                        s = calc_s(eps, x);
-                        abs_f = fabs(f - s);
-                        relative = fabs((f - s) / f);
-                        printf("%f%f%f%f", s, f, abs_f, relative);
-                    }
-                    else
-                    {
-                        rc = ERR_RANGE;
-                    }
-                }
-                else
-                {
-                    rc = ERR_IO;
-                }
-
-            }
-            else
-            {
-                rc = ERR_INPUT;
-            }
+            f = atan(x);
+            s = calc_s(eps, x);
+            abs_f = fabs(f - s);
+            relative = fabs((f - s) / f);
+            printf("%f%f%f%f", s, f, abs_f, relative);
         }
         else
         {
-            rc = ERR_IO;
+            rc = ERR_RANGE;
         }
     }
     else
     {
-        rc = ERR_INPUT;
+        rc = ERR_IO;
     }
     return rc;
 }
