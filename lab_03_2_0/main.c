3612cc2823e60cfc24a7d9281f32b5d8bd19484f
Daniil Yarockii <yarockiidaniil.mobile@gmail.com>
2018-04-28 20:00:39 +0300

    split function search

--- lab_03_2_0/main.c
+++ lab_03_2_0/main.c
@@ -1,44 +1,55 @@
 #include <stdio.h>
 #include <math.h>
 #include <errno.h>
-#include <stdlib.h>
 #include <string.h>
 
 #define RIGHT 0
-#define ERR_INP RIGHT + 1
-#define ERR_IN_FILE ERR_INP + 1
-#define ERR_OPEN ERR_IN_FILE + 1
+#define ERR_INP (RIGHT + 1)
+#define ERR_IN_FILE (ERR_INP + 1)
+#define ERR_OPEN (ERR_IN_FILE + 1)
 
-
-
-int search(FILE *file, float *number)
+int mid(FILE* file, float *cached_num, float *semi_num)
 {
-    float data_cache = 0.0, sum = 0.0, count = 0.0, semi_num = 0.0, cached_num = 0.0, pre_cached_num = 0.0;
+    float data_cache = 0.0, sum = 0.0, count = 0.0;
     while (fscanf(file, "%f", &data_cache) == 1)
     {
         count++;
         sum += data_cache;
-        if (data_cache > cached_num)
+        if (data_cache > *cached_num)
         {
-            cached_num = data_cache;
+            *cached_num = data_cache;
         }
     }
+    *semi_num = sum / count;
+    return count;
+}
+
+void sear (FILE *file, float *number, float cached_num, float count, float semi_num)
+{
+    float pre_cached_num = 0.0, data_cache = 0.0;
+    fseek(file, 0, 0);
+    for (int i = 0; i < count; i++)
+    {
+        fscanf(file, "%f", &data_cache);
+        pre_cached_num = fabs(semi_num - data_cache);
+        if (pre_cached_num < cached_num)
+        {
+            cached_num = pre_cached_num;
+            *number = data_cache;
+        }
+    }
+}
+
+
+int search(FILE *file, float *number, float *cached_num)
+{
+    float count = 0.0, semi_num = 0.0;
+    count = mid(file, cached_num, &semi_num);
     if (count == 0)
         return ERR_IN_FILE;
     else
     {
-        semi_num = sum / count;
-        fseek(file, 0, 0);
-        for (int i = 0; i < count; i++)
-        {
-            fscanf(file, "%f", &data_cache);
-            pre_cached_num = fabs(semi_num - data_cache);
-            if (pre_cached_num < cached_num)
-            {
-                cached_num = pre_cached_num;
-                *number = data_cache;
-            }
-        }
+        sear (file, number, *cached_num, count, semi_num);
         return RIGHT;
     }
 }
@@ -46,7 +57,7 @@ int search(FILE *file, float *number)
 int main(int argc, char **argv)
 {
     int err = RIGHT;
-    float number = 0.0;
+    float number = 0.0, cached_num;
     FILE *file;
 
     if (argc == 2)
@@ -54,7 +65,7 @@ int main(int argc, char **argv)
         file = fopen(argv[1], "r");
         if (file)
         {
-            err = search(file, &number);
+            err = search(file, &number, &cached_num);
             fclose(file);
         }
         else
