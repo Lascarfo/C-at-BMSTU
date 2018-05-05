4680fe675b317ebbdf351f9179dce248caa5ea34
Daniil Yarockii <yarockiidaniil.mobile@gmail.com>
2018-05-05 05:07:27 +0300

    task 4 complete

--- lab_04_4_0/main.c
+++ lab_04_4_0/main.c
@@ -3,18 +3,19 @@
 
 
 #define RIGHT 0
-#define ERR_LENGHT (RIGHT + 1)
-#define ERR_INPUT (ERR_LENGHT + 1)
+#define COMPLETE 5
+#define ERR_LENGTH (RIGHT + 1)
+#define ERR_INPUT (ERR_LENGTH + 1)
 #define MAS 10
 
 
-void sortir(int *mass, int len)
+int sortirovka(int *mass, int len)
 {
     int fcache;
     bool flag;
     for (int i = len - 1; i >= 0; i--)
     {
-        flag = 1;
+        flag = true;
         for (int j = 0; j < i; j++)
         {
             if (mass[j] > mass[j + 1])
@@ -22,38 +23,48 @@ void sortir(int *mass, int len)
                 fcache = mass[j];
                 mass[j] = mass[j + 1];
                 mass[j + 1] = fcache;
-                flag = 0;
+                flag = false;
             }
         }
-        if (flag == 1)
-            break;
+        if (flag == true)
+            return COMPLETE;
     }
+    return RIGHT;
+}
+
+int inp(int *massiv, int *length)
+{
+  int err = RIGHT, cache = 0;
+  printf("input length of mass ot 0 to 10: ");
+  scanf("%d", length);
+  if (((*length < 0) && (*length > 10)))
+  {
+      printf("pls inp int numb!\n");
+      return ERR_LENGTH;
+  }
+  printf("Input int numbers (< 10), for stop input input STRING.\n");
+  for (int i = 0; i < *length; i++)
+      if (scanf("%d", &cache) == 1)
+      {
+          massiv[i] = cache;
+      }
+      else
+      {
+          printf("pls inp int numb!\n");
+          err = ERR_INPUT;
+          return err;
+      }
+    return RIGHT;
 }
 
 int main(void)
 {
-    int err = RIGHT, cache = 0, count = 0, lenght = 0, number = 0;
-    float trash = 0.0;
-    printf("input linght of mass ot 0 to 10: ");
-    scanf("%d", &lenght);
-    if ((lenght < 0) && (lenght > 10))
-        return ERR_LENGHT;
-    int mas[MAS] = {0};
-    printf("Input int numbers (< 10), for stop input input STRING.\n");
-    for (int i = 0; i < lenght; i++)
-        if (scanf("%d", &cache) != 1)
-        {
-            printf("pls inp int numb!\n");
-            err = ERR_INPUT;
-            return err;
-        }
-        else
-        {
-            mas[i] = cache;
-        }
-    sortir(mas, lenght);
-    for (int i = 0; i < lenght; i++)
-        printf("%d ", mas[i]);
+    int err = RIGHT, length = 0, check = 0;
+    int massiv[MAS];
+    err = inp(massiv, &length);
+    check = sortirovka(massiv, length);
+    for (int i = 0; i < length; i++)
+        printf("%d ", massiv[i]);
     return err;
 }
 
