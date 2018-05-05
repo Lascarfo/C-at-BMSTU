069503bd3147a2a607f212d6b3073d63cf233ec4
Daniil Yarockii <yarockiidaniil.mobile@gmail.com>
2018-05-05 04:38:25 +0300

    task 3 complete

--- lab_04_3_0/main.c
+++ lab_04_3_0/main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 
 
 #define RIGHT 0
-#define ERR_LENGHT (RIGHT + 1)
-#define ERR_INPUT (ERR_LENGHT + 1)
+#define ERR_LENGTH (RIGHT + 1)
+#define ERR_INPUT (ERR_LENGTH + 1)
 #define MAS 10
 
 
 int polin(int number)
 {
-   int reverse = 0, cache = 0, checker = 0;
+   int reverse = 0, cache = 0;
+   bool checker = false;
    cache = number;
    while( cache != 0 )
    {
@@ -18,15 +21,15 @@ int polin(int number)
       cache = cache/10;
    }
    if ( number == reverse )
-      checker = 1;
+      checker = true;
    else
-      checker = 2;
+      checker = false;
    return checker;
 }
 
-void del(int *mas, int i)
+void del(int *mas, int i, int length)
 {
-    int pred = MAS;
+    int pred = length;
     for (; i < pred - 1; i++)
     {
       mas[i] = mas[i+1];
@@ -34,32 +37,40 @@ void del(int *mas, int i)
     }
 }
 
+int inp(int *massiv, int *length)
+{
+  int err = RIGHT, cache = 0;
+  printf("input lingth of mass ot 0 to 10: ");
+  scanf("%d", length);
+  //if (((length < 0) && (*length > 10)))
+  //    printf("pls inp int numb!\n");
+  //    return ERR_LENGTH;
+  printf("Input int numbers (< 10), for stop input input STRING.\n");
+  for (int i = 0; i < *length; i++)
+      if (scanf("%d", &cache) == 1)
+      {
+          massiv[i] = cache;
+      }
+      else
+      {
+          printf("pls inp int numb!\n");
+          err = ERR_INPUT;
+          return err;
+      }
+    return RIGHT;
+}
+
+
 int main(void)
 {
-    int err = RIGHT, cache = 0, count = 0, lenght = 0, number = 0;
-    float trash = 0.0;
-    printf("input linght of mass ot 0 to 10: ");
-    scanf("%d", &lenght);
-    if ((lenght < 0) && (lenght > 10))
-        return ERR_LENGHT;
-    int mas[MAS] = {0};
-    printf("Input int numbers (< 10), for stop input input STRING.\n");
-    for (int i = 0; i < lenght; i++)
-        if (scanf("%d", &cache) != 1)
-        {
-            printf("pls inp int numb!\n");
-            err = ERR_INPUT;
-            return err;
-        }
-        else
-        {
-            mas[i] = cache;
-        }
-    for (int i = 0; i < lenght; i++)
-        if (polin(mas[i]) == 1)
-            del(mas, i);
-    for (int i = 0; i < lenght; i++)
-        printf("%d ", mas[i]);
+    int err = RIGHT, length = 0;
+    int massiv[MAS];
+    err = inp(massiv, &length);
+    for (int i = 0; i < length; i++)
+        if (polin(massiv[i]) == true)
+            del(massiv, i, length);
+    for (int i = 0; i < length; i++)
+        printf("%d ", massiv[i]);
     return err;
 }
 
