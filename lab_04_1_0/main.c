9329a32e52d2506fcb7d78a8507d1d5fcd6138a7
Daniil Yarockii <yarockiidaniil.mobile@gmail.com>
2018-05-05 04:08:54 +0300

    fully correct task 1

--- lab_04_1_0/main.c
+++ lab_04_1_0/main.c
@@ -2,48 +2,55 @@
 
 
 #define RIGHT 0
-#define ERR_LENGHT (RIGHT + 1)
-#define ERR_INPUT (ERR_LENGHT + 1)
+#define ERR_LENGTH (RIGHT + 1)
+#define ERR_INPUT (ERR_LENGTH + 1)
 #define MAS 10
 
 
-int counter(int mas[MAS],int lenght, int *number)
+int counter(int const mas[MAS],int length, int *number)
 {
     int err = RIGHT;
-    for (int i = 0; i < lenght; i++)
+    for (int i = 0; i < length; i++)
     {
         if ((mas[i] % 2) == 0)
+        {
             *number += mas[i];
+        }
     }
     return err;
 }
 
-
+int inp(int *massiv, int *length)
+{
+  int err = RIGHT, cache = 0;
+  printf("input lingth of mass ot 0 to 10: ");
+  scanf("%d", length);
+  if (((length < 0) && (*length > 10)) == 0)
+      printf("pls inp int numb!\n");
+      return ERR_LENGTH;
+  printf("Input int numbers (< 10), for stop input input STRING.\n");
+  for (int i = 0; i < *length; i++)
+      if (scanf("%d", &cache) == 1)
+      {
+          massiv[i] = cache;
+      }
+      else
+      {
+          printf("pls inp int numb!\n");
+          err = ERR_INPUT;
+          return err;
+      }
+    return RIGHT;
+}
 
 int main(void)
 {
-    int err = RIGHT, cache = 0, count = 0, lenght = 0, number = 0;
-    float trash = 0.0;
-    printf("input linght of mass ot 0 to 10: ");
-    scanf("%d", &lenght);
-    if ((lenght < 0) && (lenght > 10))
-        return ERR_LENGHT;
-    int massiv[MAS] = {0};
-    printf("Input int numbers (< 10), for stop input input STRING.\n");
-    for (int i = 0; i < lenght; i++)
-        if (scanf("%d", &cache) != 1)
-        {
-            printf("pls inp int numb!\n");
-            err = ERR_INPUT;
-            return err;
-        }
-        else
-        {
-            massiv[i] = cache;
-        }
-    err = counter(massiv, lenght, &number);
+    int err = RIGHT, length = 0, number = 0;
+    int massiv[MAS];
+    err = inp(massiv, &length);
+    if (err == 0)
+        err = counter(massiv, length, &number);
     if (err == 0)
         printf("summ is %d\n", number);
     return err;
 }
-
