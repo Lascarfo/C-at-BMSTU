fb1ee763ed37bf905e0dd2cfe56f89551080dea1
Daniil Yarockii <yarockiidaniil.mobile@gmail.com>
2018-05-18 01:33:26 +0300

    General fix task 2

--- lab_02_3_2/main.c
+++ lab_02_3_2/main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
+#include <math.h>
 
 #define HL 0
-#define ERR_DIG -1
-#define OL HL + 1
-#define DL OL + 1
+#define ERR_DIG 5
+#define OL (HL + 1)
+#define DL (OL + 1)
+#define EPS 1e-6f
 
 int line(float x, float x1, float x2, float y, float y1, float y2)
 {
-    int rcc;
-    if (((y1 - y2) * x + (x2 - x1) * y + (x1 * y2 - x2 * y1)) == 0)
+    int rcc = ((y1 - y2) * x + (x2 - x1) * y + (x1 * y2 - x2 * y1));
+    if (rcc == 0)
         rcc = OL;
-    if (((y1 - y2) * x + (x2 - x1) * y + (x1 * y2 - x2 * y1)) > 0)
+    else if (rcc > 0)
         rcc = HL;
-    if (((y1 - y2) * x + (x2 - x1) * y + (x1 * y2 - x2 * y1)) < 0)
+    else if (rcc < 0)
         rcc = DL;
     return rcc;
 }
@@ -23,7 +25,7 @@ int main(void)
     float x, x1, x2, y, y1, y2;
     if (scanf("%f %f %f %f %f %f", &x1, &y1, &x2, &y2, &x, &y) == 6)   
     {
-        if ((x1 == x2) && (y2 == y1))
+        if ((fabs(x1 - x2) <= EPS) && (fabs(y1 - y2) <= EPS))
         {
             rc = ERR_DIG;
         }
