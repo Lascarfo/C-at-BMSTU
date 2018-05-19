744d3ebd45dfd56c7022367976fcc350bc495b40
U-VLAB\user-lab01 <user-lab01@LAB-01-55.vlab.bmstu.ru>
2018-05-19 18:33:18 +0300

    final push

--- RK_22_1/test.c
+++ RK_22_1/test.c
@@ -10,60 +10,73 @@ int main(void)
         FILE *file2;
         int cache = 0;
         file1 = fopen ("file1.txt", "r");
+        file2 = fopen ("file2.txt", "r");
         if (file1)
         {
             if (fscanf(file1, "%d", &cache) != 1)
             {
-                fprintf(stdout, "no such numbers in file1");
+                fprintf(stdout, "no such numbers in file1\n");
                 return ERR_FILE;
             }
             else
             {
                 if (cache > 0)
                 {
-
                 }
                 else
                 {
-                    fprintf(stdout, "len < 1");
+                    fprintf(stdout, "len < 1\n");
                     return ERR_FILE;
                 }
             }
         }
         else
         {
-            fprintf(stdout, "Cannot open file 1");
+            fprintf(stdout, "Cannot open file 1\n");
             return ERR_FILE;
         }
         if (file2)
         {
             if (fscanf(file2, "%d", &cache) != 1)
             {
-                fprintf(stdout, "no such numbers in file2");
+                fprintf(stdout, "no such numbers in file2\n");
                 return ERR_FILE;
             }
             else
             {
                 if (cache > 0)
                 {
-
+                    fprintf(stdout, "All test with files are ok!\n");
                 }
                 else
                 {
-                    fprintf(stdout, "len < 1");
+                    fprintf(stdout, "len < 1\n");
                     return ERR_FILE;
                 }
             }
         }
         else
         {
-            fprintf(stdout, "Cannot open file 2");
+            fprintf(stdout, "Cannot open file 2\n");
             return ERR_FILE;
         }
 
     }
-    {                                                   //test func pick_f
-
+    {
+        int number = 0;                                                    //test func pick_f
+        int x[] = {1, 2, 8, 5, 0, 2, 9, 0, 5};
+        int y[] = {3, -4, 0, 7, 2};
+        int n1 = 9, n2 = 5;
+        pick(x, y, &number, n1, n2);
+        if (number == RIGHT)
+        {
+            fprintf(stdout, "pick_f test are ok!\n");
+        }
+        else
+        {
+            fprintf(stdout, "pick_f test are not ok!\n");
+            return ERR_ARR;
+        }
     }
     return RIGHT;
 }
