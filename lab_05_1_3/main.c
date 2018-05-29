30a2d40338f7107f891f18947ed2554362b1aac8
Daniil Yarockii <yarockiidaniil.mobile@gmail.com>
2018-05-29 04:54:24 +0300

    general fix

--- lab_05_1_3/main.c
+++ lab_05_1_3/main.c
@@ -19,42 +19,45 @@ void usage(void)
     fprintf(stdout, "Please make input following next instructions:\nmain.exec main.txt\n");
 }
 
-void minimum(int *number, int *pb, int *pe)
+void minimum(int *pb, int *pe)
 {
-    int *end = pe - 1, *pb2 = pb + 1;
+    int *end = pe - 1, *pb2 = pb + 1, number = (*pb) * (*pb2);
     while (pb < end)
     {
-        if ((*number) >  ((*pb)*(*pb2)))
+        if ((number) >  ((*pb)*(*pb2)))
         {
-            *number = (*pb)*(*pb2);
+            number = (*pb)*(*pb2);
         }
     pb++;
     pb2++;
     }
+    fprintf(stdout, "MIN number is: %d\n", number);
 }
 
 
-int field(FILE* file, int *arr, int *number)
+int field(FILE* file, int *arr)
 {
     int cache = 0;
     int *pb = arr;
     int *pe = arr;
-    for (int i = 0; i < ARR; i++)
+    while (fscanf(file, "%d", &cache) == 1)
     {
-        if (fscanf(file, "%d", &cache) == 1)
-        {
-            *pe = cache;
-            pe++;
-        }
+        *pe = cache;
+        pe++;
     }
-    minimum(number, pb, pe);
+    if ((pe - pb) < 2)
+    {
+            printf("err in file");
+            return ERR_INPUT;
+    }
+    else
+        minimum(pb, pe);
     return OK;
 }
 
 int main(int argc, char** argv)
 {
     int err = OK;
-    int number = INT_MAX;
     int arr[ARR] = {0};
     FILE* file;
     if (argc == 2)
@@ -62,7 +65,8 @@ int main(int argc, char** argv)
         file = fopen(argv[1], "r");
         if (file)
         {
-            err = field(file, arr, &number);
+            err = field(file, arr);
+            fclose(file);
         }
         else
         {
@@ -74,13 +78,6 @@ int main(int argc, char** argv)
         usage();
         err = ERR_OFILE;
     }
-    if (number == INT_MAX)
-    {
-        err = ERR_INPUT;
-    }
-    else
-    {
-        fprintf(stdout, "MIN number is: %d\n", number);
-    }
+
     return err;
 }
