1bb79d4db7a971cd57cd37acc36b7d4273c56b69
qanelph <yarockiidaniil.mobile@gmail.com>
2018-09-24 16:42:35 +0300

    push new main

--- lab_07_3/main.c
+++ lab_07_3/main.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
     int rc = OK;
     FILE *file_in;
     FILE *file_out;
-    if (argc == 3)
+    if (((argc == 3) || ((argc == 4) && (strcmp(argv[3], "f")) == 0)))
     {
         file_in = fopen(argv[1], "r");
         if (file_in)
@@ -44,52 +44,33 @@ int main(int argc, char *argv[])
             file_out = fopen(argv[2], "w");
             if (file_out)
             {
-                int *arr_s = NULL, *arr_s_end = NULL;
-                rc = read(file_in, &arr_s, &arr_s_end);
-                if (rc == OK)
+                if (argc == 4)
                 {
-                    mysort((arr_s), (arr_s_end - arr_s), sizeof(int), cmp_int);
-                    save(file_out, arr_s, arr_s_end);
-                    free(arr_s);
+                    int *arr = NULL, *arr_end = NULL;
+                    rc = read(file_in, &arr, &arr_end);
+                    if (rc == OK)
+                    {
+                        int *arr_s = NULL, *arr_s_end = NULL;
+                        rc = key(arr, arr_end, &arr_s, &arr_s_end);  // вызов функции - фильтра
+                        if (rc == OK)
+                        {
+                            mysort((arr_s), (arr_s_end - arr_s), sizeof(int), cmp_int);
+                            save(file_out, arr_s, arr_s_end);
+                        }
+                        free(arr_s);
+                        free(arr);
+                    }
                 }
-                fclose(file_out);
-            }
-            else
-            {
-                fprintf(stdout, "Problems with open file main_out.txt\n");
-                usage();
-                rc = ERR_FILE;
-            }
-            fclose(file_in);
-        }
-        else
-        {
-            fprintf(stdout, "Problems with open file main_in.txt\n");
-            usage();
-            rc = ERR_FILE;
-        }
-    }
-    else if ((argc == 4) && (strcmp(argv[3], "f") == 0))
-    {
-        file_in = fopen(argv[1], "r");
-        if (file_in)
-        {
-            file_out = fopen(argv[2], "w");
-            if (file_out)
-            {
-                int *arr = NULL, *arr_end = NULL;
-                rc = read(file_in, &arr, &arr_end);
-                if (rc == OK)
+                else
                 {
                     int *arr_s = NULL, *arr_s_end = NULL;
-                    rc = key(arr, arr_end, &arr_s, &arr_s_end);  // вызов функции - фильтра
+                    rc = read(file_in, &arr_s, &arr_s_end);
                     if (rc == OK)
                     {
                         mysort((arr_s), (arr_s_end - arr_s), sizeof(int), cmp_int);
                         save(file_out, arr_s, arr_s_end);
+                        free(arr_s);
                     }
-                    free(arr_s);
-                    free(arr);
                 }
                 fclose(file_out);
             }
