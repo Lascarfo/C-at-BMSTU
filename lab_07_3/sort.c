1681111a96bdc9b1fa1b0f08c4b9ce3bb2d7efae
qanelph <yarockiidaniil.mobile@gmail.com>
2018-09-30 02:39:42 +0300

    try to fix sort.c (2)

--- lab_07_3/sort.c
+++ lab_07_3/sort.c
@@ -51,7 +51,7 @@ int cmp_int(const void *left, const void *right)
 
 void mysort(void *base, size_t nitems, size_t size, int(*cmpr)(const void*, const void*))
 {
-    if (true)
+    if (nitems != 0)
     {
         char *arr_s = base;
         char *arr_s_end = arr_s + nitems * size - size;
@@ -62,7 +62,7 @@ void mysort(void *base, size_t nitems, size_t size, int(*cmpr)(const void*, cons
         bool even = false;
 
 
-        while (end > start)
+        while (end => start)
         {
             even = !even;
             if (even)
