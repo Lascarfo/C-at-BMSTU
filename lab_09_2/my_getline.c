58f60cd86085542f7c3075b9ec9f000982e784cd
qanelph <yarockiidaniil.mobile@gmail.com>
2018-11-05 19:22:11 +0300

    try to fix 2

--- lab_09_2/my_getline.c
+++ lab_09_2/my_getline.c
@@ -32,7 +32,6 @@ int my_getline(char **lineptr, size_t *n, FILE *stream)
                 sym_count = str_len(*lineptr);
                 if (end_of_line(*lineptr, sym_count, &length))
                 {
-                    *lineptr = realloc(*lineptr, length + 1);
                     return sym_count;
                 }
                 *n += BUFFER;
--- lab_09_2/test.c
+++ lab_09_2/test.c
@@ -19,26 +19,26 @@
  * \details сравнивает две строки, поступившие на вход
  */
 
- const bool cmp_strings(const char *first, const char *second)
- {
-     int count = 0;
-     while (second[count] != '\0' || first[count] != '\0')
-     {
-         if (first[count] != second[count])
-         {
-             return false;
-         }
-         count++;
-     }
-     if (second[count] == '\0' && first[count] == '\0')
-     {
-         return true;
-     }
-     else
-     {
-         return false;
-     }
- }
+const bool cmp_strings(const char *first, const char *second)
+{
+    int count = 0;
+    while (second[count] != '\0' || first[count] != '\0')
+    {
+        if (first[count] != second[count])
+        {
+            return false;
+        }
+        count++;
+    }
+    if (second[count] == '\0' && first[count] == '\0')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
 /**
 * \brief основная функция программы test.c
